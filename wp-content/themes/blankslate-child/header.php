8325194381fb65ff620c527954f0e38baf7d8ee1
Isabelle.Ancelin <77393135+isabelle-ancelin@users.noreply.github.com>
2023-06-23 16:19:24 +0200

    nouvelles modifs

--- wp-content/themes/blankslate-child/header.php
+++ wp-content/themes/blankslate-child/header.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
-<html <?php language_attributes(); ?> >
+<html <?php language_attributes(); ?>>
 
 <head>
     <meta charset="<?php bloginfo('charset'); ?>" />
     <meta name="viewport" content="width=device-width" />
     <?php wp_head(); ?>
-    
+
 </head>
 
 <body <?php body_class(); ?>>
     <?php wp_body_open(); ?>
     <div id="wrapper" class="hfeed">
-        <header id="header" role="banner">
+        <header id="header" role="banner" class="custom-header">
             <div id="branding">
                 <div id="site-title" itemprop="publisher" itemscope itemtype="https://schema.org/Organization">
                     <?php
@@ -30,6 +30,7 @@
             </div>
             <nav id="menu" role="navigation" itemscope itemtype="https://schema.org/SiteNavigationElement">
                 <?php wp_nav_menu(array('theme_location' => 'main-menu', 'link_before' => '<span itemprop="name">', 'link_after' => '</span>')); ?>
+                
             </nav>
         </header>
         <div id="container">
