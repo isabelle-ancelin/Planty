ead36074f97eeaae8318f48e193d36ae60161ada
Isabelle.Ancelin <77393135+isabelle-ancelin@users.noreply.github.com>
2023-07-04 16:06:33 +0200

    mise à jour des formulaires et du header

--- wp-content/themes/Beans-child/functions.php
+++ wp-content/themes/Beans-child/functions.php
@@ -4,4 +4,6 @@ function theme_enqueue_styles()
 {
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
     wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', array(), filemtime(get_stylesheet_directory() . '/css/theme.css'));
-}
+};
+// Remove the breadcrumb.
+add_filter( 'beans_pre_load_fragment_breadcrumb', '__return_true' );
